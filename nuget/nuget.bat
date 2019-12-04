a1d65bcc82c23549fb111c6cf75aeee2f9e27674
Dale Newman <dalenewman@gmail.com>
2019-12-04 14:57:57 -0500

    Updated dependencies

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -3,11 +3,11 @@ nuget pack Transformalize.Transform.Jint.Autofac.nuspec -OutputDirectory "c:\tem
 nuget pack Transformalize.Validate.Jint.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Validate.Jint.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Transform.Jint.0.6.4-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Jint.Autofac.0.6.4-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Jint.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Jint.Autofac.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM 
-REM nuget push "c:\temp\modules\Transformalize.Validate.Jint.0.6.4-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Validate.Jint.Autofac.0.6.4-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Validate.Jint.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Validate.Jint.Autofac.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
