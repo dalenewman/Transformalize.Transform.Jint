5ee50df8038d376c1f6e446ff1dfefdbfec5d254
Dale Newman <dalenewman@gmail.com>
2020-04-19 23:27:39 -0400

    fix nuspec standard 2.1 to 2.0

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,13 +1,13 @@
-nuget pack Transformalize.Transform.Jint.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Transform.Jint.Autofac.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Validate.Jint.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Validate.Jint.Autofac.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Transform.Jint.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Transform.Jint.Autofac.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Validate.Jint.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Validate.Jint.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Transform.Jint.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Jint.Autofac.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Jint.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Jint.Autofac.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM 
-REM nuget push "c:\temp\modules\Transformalize.Validate.Jint.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Validate.Jint.Autofac.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Validate.Jint.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Validate.Jint.Autofac.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
