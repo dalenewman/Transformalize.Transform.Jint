4496bf2f00210dd65e53c9cf22a0a6235afe4d1d
Dale Newman <dalenewman@gmail.com>
2020-04-23 16:46:35 -0400

    pushed out jint 3+ version packages

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,7 +1,7 @@
-nuget pack Transformalize.Transform.Jint.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Transform.Jint.Autofac.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Validate.Jint.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Validate.Jint.Autofac.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Transform.Jint.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Transform.Jint.Autofac.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Validate.Jint.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Validate.Jint.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 REM Jint v3+ Ones right-click and pack
 
 REM nuget push "c:\temp\modules\Transformalize.Transform.Jint.0.6.30-beta.nupkg" -source https://api.nuget.org/v3/index.json
@@ -10,6 +10,11 @@ REM
 REM nuget push "c:\temp\modules\Transformalize.Validate.Jint.0.6.30-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Validate.Jint.Autofac.0.6.30-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
+nuget push "c:\temp\modules\Transformalize.Transform.Jint.3.0.6.30-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Jint.3.Autofac.0.6.30-beta.nupkg" -source https://api.nuget.org/v3/index.json
+
+nuget push "c:\temp\modules\Transformalize.Validate.Jint.3.0.6.30-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Validate.Jint.3.Autofac.0.6.30-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
