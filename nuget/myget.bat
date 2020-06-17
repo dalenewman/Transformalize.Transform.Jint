e04ecd1cda865f432a4437d6eff624576b8fb0ca
Dale Newman <dalenewman@gmail.com>
2020-06-17 00:39:03 -0400

    Updated for Transformalize 0.8.1

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -8,17 +8,17 @@ REM nuget pack Transformalize.Transform.Jint.3.Autofac.nuspec -OutputDirectory "
 REM nuget pack Transformalize.Validate.Jint.3.nuspec -OutputDirectory "c:\temp\modules"
 REM nuget pack Transformalize.Validate.Jint.3.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-nuget push "c:\temp\modules\Transformalize.Transform.Jint.0.7.18-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Transform.Jint.Autofac.0.7.18-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Jint.0.8.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Jint.Autofac.0.8.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
-nuget push "c:\temp\modules\Transformalize.Validate.Jint.0.7.17-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Validate.Jint.Autofac.0.7.17-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Validate.Jint.0.8.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Validate.Jint.Autofac.0.8.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
-nuget push "c:\temp\modules\Transformalize.Transform.Jint.3.0.7.18-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Transform.Jint.3.Autofac.0.7.18-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Jint.3.0.8.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Jint.3.Autofac.0.8.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
-nuget push "c:\temp\modules\Transformalize.Validate.Jint.3.0.7.17-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Validate.Jint.3.Autofac.0.7.17-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Validate.Jint.3.0.8.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Validate.Jint.3.Autofac.0.8.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
 
 
