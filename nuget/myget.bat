d3a5c20792e4d7dcb12516ca55ee44a7961bd542
Dale Newman <dalenewman@gmail.com>
2025-01-12 13:13:16 -0500

    - bump myget package versions

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -3,8 +3,8 @@ nuget pack Transformalize.Transform.Jint.Autofac.nuspec -OutputDirectory "c:\tem
 nuget pack Transformalize.Validate.Jint.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Validate.Jint.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-nuget push "c:\temp\modules\Transformalize.Transform.Jint.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Transform.Jint.Autofac.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Jint.0.11.2-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Jint.Autofac.0.11.2-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
-nuget push "c:\temp\modules\Transformalize.Validate.Jint.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Validate.Jint.Autofac.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Validate.Jint.0.11.2-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Validate.Jint.Autofac.0.11.2-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
